4df2a3fc68a4beeaac5ce95a8f1eada094947465
damien.galban <damien.galban@lumiscaphe.com>
2016-04-14 13:59:46 +0200

    Continue tutorial. stopped at route definition

--- passport-mongo/passport/signup.js
+++ passport-mongo/passport/signup.js
@@ -1,23 +1,52 @@
-var login = require('./login');
-var signup = require('./signup');
-var User = require ('../models/user');
-
 module.exports = function(passport){
-    // Passport needs to be able to serialize and deserialize users to support persistent login sessions
-    passport.serializeUser(function(user, done) {
-        console.log('serializing user: ');
-        console.log(user);
-        done(null, user._id);
-    });
-
-    passport.deserializeUser(function(id, done) {
-        User.findById(id, function(err, user) {
-            console.log('deserializing user:',user);
-            done(err, user);
-        });
-    });
-
-    // Setting up Passport Strategies for Login and SignUp/Registration
-    login(passport);
-    signup(passport);  
+    passport.use('signup', new LocalStrategy({
+        passReqToCallback : true
+    },
+      function(req, username, password, done)  {
+          findOrCreateUser = function(){
+              //find a user in Mongo with provided username
+              User.findOne({'username':username},function(err, user) {
+                  // In case of any error return
+                  if (err){
+                      console.log('Error in Signup: '+err);
+                      return done(err);
+                  }
+                  // already exists
+                  if (user) {
+                      console.log('User already exists');
+                      return done(null, false,
+                          req.flash('message', 'User Already Exists.'));
+                  } else {
+                      //if there is no user with that email
+                      //create the user
+                      var newUser = new User();
+                      // set the user's local credentials
+                      newUser.username = username;
+                      newUser.password = createHash(password);
+                      newUser.email = req.param('email');
+                      newUser.firstname = req.param('firstName');
+                      newUser.lastName = req.param('lastName');
+                      
+                      //save the user
+                      newUser.Save(function(err) {
+                          if (err) {
+                              console.log('Error in Saving user: '+err);
+                              throw err;
+                          }
+                          console.log('User Registration succesful');
+                          return done(null, newUser);
+                      });
+                  }
+              });
+          };
+          // Delay the execution of findOrCreateUser and execute the method
+          // in the next tick of the event loop
+          process.nextTick(findOrCreateUser);
+      })
+    );
+    
+    // Generates hash using bCrypt
+    var createHash = function(password){
+    return bCrypt.hashSync(password, bCrypt.genSaltSync(10), null);
+  }
 }
